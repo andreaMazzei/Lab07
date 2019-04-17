56d4f355223548cec14ab6ba112b7568a917f398
S226203 <S226203@lepcopi44.cdsdom.polito.it>
2019-04-17 13:33:30 +0200

    GUI and comboBox

--- src/it/polito/tdp/poweroutages/PowerOutagesController.java
+++ src/it/polito/tdp/poweroutages/PowerOutagesController.java
@@ -1,6 +1,59 @@
 package it.polito.tdp.poweroutages;
 
-public class PowerOutagesController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import it.polito.tdp.poweroutages.model.Model;
+import it.polito.tdp.poweroutages.model.Nerc;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class PowerOutagesController {
 	
+	private Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<Nerc> comboNerc;
+
+    @FXML
+    private TextField txtYears;
+
+    @FXML
+    private TextField txtHours;
+
+    @FXML
+    private Button btnAnalysis;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doAnalysis(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert comboNerc != null : "fx:id=\"comboNerc\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+        assert txtYears != null : "fx:id=\"txtYears\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+        assert txtHours != null : "fx:id=\"txtHours\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+        assert btnAnalysis != null : "fx:id=\"btnAnalysis\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'PowerOutages.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+		comboNerc.getItems().addAll(model.getNercList());
+	}
 }
